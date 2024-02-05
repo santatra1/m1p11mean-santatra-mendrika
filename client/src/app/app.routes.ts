c5a328b400b36ed692b7e675bd7fe5df86c75626
Mendrika1284 <68342672+Mendrika1284@users.noreply.github.com>
2024-02-05 23:13:42 +0300

    💄 Mise en place template backoffice

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ServiceComponent } from './clients/service/service.component';
-
+import { WelcomeComponent } from './manager/welcome/welcome.component';
 
 export const routes: Routes = [
-    
   { path: 'service', component: ServiceComponent },
-  {path: '', component: LoginComponent},
-  {path: 'signup', component: SignupComponent}
+  { path: '', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'manager', component: WelcomeComponent },
 ];
