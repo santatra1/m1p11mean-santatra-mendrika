7ae123ca22267221587c17b0a35b5fc8b8a1479a
santatra1 <niavo440@gmail.com>
2024-02-05 11:40:30 +0300

    Template client intégré

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink, RouterLinkActive } from '@angular/router';
+import { NavbarComponent } from './clients/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ServiceComponent } from './clients/service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'service', component: ServiceComponent },
+];
