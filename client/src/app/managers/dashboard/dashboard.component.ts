181d6ed16f2fd17712bad27a5c2ab36618a31caa
Mendrika1284 <68342672+Mendrika1284@users.noreply.github.com>
2024-02-26 16:23:29 +0300

    ✨ Integration chart

--- client/src/app/managers/dashboard/dashboard.component.ts
+++ client/src/app/managers/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
-
+import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -9,5 +9,50 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  chart: any = []
+  chart2: any = []
 
+  ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+    this.chart2 = new Chart('canvas2', {
+      type: 'doughnut',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+  
 }
